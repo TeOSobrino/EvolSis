dc5541aa2ed66f59200dbc7118e4784e3a98f282
Carlos Craveiro <CarlosCraveiro@usp.br>
2023-12-28 23:51:15 -0300

    updates the default shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ gnumake boost ];
-    buildInputs = with pkgs.buildPackages; [ python3 ];
+    nativeBuildInputs = with pkgs.buildPackages; [ gnumake boost eigen ];
+    buildInputs = with pkgs.buildPackages; [ python3 gnuplot zsh ];
+
+    # Setting zsh as the dafault shell
+    shellHook = ''
+        export SHELL=${pkgs.zsh}/bin/zsh
+    '';
 }
